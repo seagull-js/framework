454bc82c54829be7cfb5bbde912eda5cb0f7623b
Maximilian Stroh <Hisako1337@gmail.com>
2018-04-01 15:05:30 +0200

    moved doc snippet

--- src/parrots/README.md
+++
@@ -1,6 +0,0 @@
-# Parrot
-
-Strictly speaking, a [parrot](http://www.dictionary.com/browse/parrot) is a
-bird that mimics speech and other things. In this case, parrots are mock
-implementations for certain AWS services for local usage within tests or the
-local development server. They are obviously not seagulls, but mimic behavior.
--- src/parrots/index.ts
+++ src/parrots/index.ts
@@ -1,1 +1,11 @@
+/**
+ * # Parrot
+ *
+ * Strictly speaking, a [parrot](http://www.dictionary.com/browse/parrot) is a
+ * bird that mimics speech and other things. In this case, parrots are mock
+ * implementations for certain AWS services for local usage within tests or the
+ * local development server. They are obviously not seagulls, but mimic
+ * specific behavior, mostly from AWS Services.
+ */
+
 export * from './simple_db'
